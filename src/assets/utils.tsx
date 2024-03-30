ce909b9177ba2916b515399897d1fca3373f6a3c
Pedro Henrique <63562843+pedrocorrea2002@users.noreply.github.com>
2024-03-30 01:57:02 -0300

    Update utils.tsx

--- src/assets/utils.tsx
+++ src/assets/utils.tsx
@@ -1,6 +1,6 @@
 //! ///// DATAS ///////////////////////////
 export const monthList = {
-    0: "Janeiro", 1: "Feveiro", 2: "Março", 3: "Abril", 4: "Maio", 5: "Junho", 6: "Julho", 7: "Agosto", 8: "Setembro", 9: "Outubro", 10: "Novembro", 11: "Dezembro"
+    0: "Janeiro", 1: "Fevereiro", 2: "Março", 3: "Abril", 4: "Maio", 5: "Junho", 6: "Julho", 7: "Agosto", 8: "Setembro", 9: "Outubro", 10: "Novembro", 11: "Dezembro"
 }
 
 //? CONVERTE Epoch PARA Date
